9175917c51565be3e2cdb5607f43036e75e52bb2
ermiry <erick.salas@ermiry.com>
2021-06-12 23:52:16 -0500

    Version 0.1

--- include/version.h
+++ include/version.h
@@ -3,9 +3,9 @@
 
 #define BRAWL_VERSION				"0.1"
 #define BRAWL_VERSION_NAME			"Version 0.1"
-#define BRAWL_VERSION_DATE			"28/05/2021"
-#define BRAWL_VERSION_TIME			"23:30 CST"
-#define BRAWL_VERSION_AUTHOR			"Erick Salas"
+#define BRAWL_VERSION_DATE			"12/06/2021"
+#define BRAWL_VERSION_TIME			"23:52 CST"
+#define BRAWL_VERSION_AUTHOR		"Erick Salas"
 
 // print full brawl version information
 extern void brawl_version_print_full (void);
